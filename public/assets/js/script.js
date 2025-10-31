be00c2b7bd26fa0710ae4aac3521a2c7262eab23
maryh12 <103159989+maryh12@users.noreply.github.com>
2025-10-31 14:43:11 -0300

    fix: Definindo que só pode selecionar a data de nascimento até o dia atual.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
   $('.cellphone').mask('(00) 00000-0000');
   $('.phone').mask('(00) 0000-0000');
+
+  const hoje = new Date();
+
+  //Ajuste do fuso para o fuso local
+  const diferencaFuso = hoje.getTimezoneOffset(); //diferença do fuso em minutos para o UTC
+  hoje.setMinutes(hoje.getMinutes() - diferencaFuso);
+
+  const dataLocal = hoje.toISOString().split('T')[0];
+
+  document.getElementById('birth_date').setAttribute('max', dataLocal);
 });
